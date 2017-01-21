ba70af19d39ffbca07c23d36c3f05d48ca6c7ac2
KyluAce <kyluace@gmail.com>
2017-01-21 19:52:51 +0100

    first commit

---
+++ app/src/main/java/com/example/kylu/enigma/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.kylu.enigma;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity {
+
+    public Button login;
+    public Button pass;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
